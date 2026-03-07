0b08957f3b13620488fdf8c5a5fc16222672d5ba
DuyKhanh2004tc <duykhanh2004tcmc@gmail.com>
2026-03-08 02:55:19 +0700

    finish scene 3

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    
+    public void NextScene()
+    {
+        
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("Finish");
+        }
+    }
+}
